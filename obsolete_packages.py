53fde653c41dc6dfc3b101d9a0b18430dfb7ce7c
Miro Hrončok <miro@hroncok.cz>
2019-11-27 16:54:11 +0100

    Rework the obsolete script to use dnf API

--- obsolete_packages.py
+++ obsolete_packages.py
@@ -1,7 +1,7 @@
-import json
+import dnf
+import re
 import subprocess
 import sys
-import time
 from collections import defaultdict
 
 
@@ -9,48 +9,90 @@ FIRST = 14
 EOL = 29
 RAWHIDEVER = 32
 
-try:
-    with open('repoquery_cache.json', 'r') as f:
-        cache = json.load(f)
-except FileNotFoundError:
-    cache = {}
+DNF_CACHEDIR = '_dnf_cache_dir'
+ARCH = 'x86_64'
+
+INTSTART = re.compile(r'^(\d+).+')
+
+sacks = {}
+
+# modularity problems :(
+# https://bugzilla.redhat.com/show_bug.cgi?id=1636285
+excludepkgs = ('fedora-release,fedora-release-cinnamon,fedora-release-cloud,'
+               'fedora-release-container,fedora-release-coreos,'
+               'fedora-release-iot,fedora-release-kde,'
+               'fedora-release-matecompiz,fedora-release-server,'
+               'fedora-release-silverblue,fedora-release-snappy,'
+               'fedora-release-soas,fedora-release-workstation,'
+               'fedora-release-xfce,generic-release')
+
+
+def rawhide_sack():
+    try:
+        return sacks[None]
+    except KeyError:
+        pass
+    base = dnf.Base()
+    conf = base.conf
+    conf.cachedir = DNF_CACHEDIR
+    conf.substitutions['releasever'] = str(RAWHIDEVER)
+    conf.substitutions['basearch'] = ARCH
+    base.repos.add_new_repo('rawhide', conf,
+        metalink='https://mirrors.fedoraproject.org/metalink?repo=rawhide&arch=$basearch',
+        skip_if_unavailable=False,
+        enabled=True,
+        excludepkgs=excludepkgs)
+    base.fill_sack(load_system_repo=False, load_available_repos=True)
+    sacks[None] = base.sack
+    return base.sack
+
+
+def fedora_sack(version):
+    try:
+        return sacks[version]
+    except KeyError:
+        pass
+    base = dnf.Base()
+    conf = base.conf
+    conf.cachedir = DNF_CACHEDIR
+    conf.substitutions['releasever'] = str(version)
+    conf.substitutions['basearch'] = ARCH
+    base.repos.add_new_repo(f'fedora{version}', conf,
+        metalink='https://mirrors.fedoraproject.org/metalink?repo=fedora-$releasever&arch=$basearch',
+        skip_if_unavailable=False,
+        enabled=True,
+        excludepkgs=excludepkgs)
+    base.repos.add_new_repo(f'updates{version}', conf,
+        metalink='https://mirrors.fedoraproject.org/metalink?repo=updates-released-f$releasever&arch=$basearch',
+        skip_if_unavailable=False,
+        enabled=True,
+        excludepkgs=excludepkgs)
+    base.repos.add_new_repo(f'updates-testing{version}', conf,
+        metalink='https://mirrors.fedoraproject.org/metalink?repo=updates-testing-f$releasever&arch=$basearch',
+        skip_if_unavailable=False,
+        enabled=True,
+        excludepkgs=excludepkgs)
+    base.fill_sack(load_system_repo=False, load_available_repos=True)
+    sacks[version] = base.sack
+    return base.sack
 
 
 def repoquery(*args, **kwargs):
-    cmd = ['repoquery']
     version = kwargs.pop('version', None)
     if version is None:
-        cmd.append('--repo=rawhide')
+        sack = rawhide_sack()
     else:
-        cmd.extend(['--repo=fedora', '--repo=updates',
-                    '--repo=updates-testing', f'--releasever={version}'])
-    if args:
-        cmd.extend(args)
-    for option, value in kwargs.items():
-        cmd.append(f'--{option}')
-        if value is not True:
-            cmd.append(value)
-    if version is not None and version <= EOL:
-        try:
-            return cache[str(cmd)]
-        except KeyError:
-            pass
-    while True:
-        try:
-            proc = subprocess.run(cmd,
-                                  text=True,
-                                  stdout=subprocess.PIPE,
-                                  stderr=subprocess.DEVNULL,
-                                  check=True)
-        except subprocess.CalledProcessError:
-            print('! repoquery failed, retrying in 5 seconds', file=sys.stderr)
-            time.sleep(5)
-        else:
-            if version is not None and version <= EOL:
-                cache[str(cmd)] = proc.stdout.splitlines()
-                with open('repoquery_cache.json', 'w') as f:
-                    json.dump(cache, f, indent=4)
-            return proc.stdout.splitlines()
+        sack = fedora_sack(version)
+    if 'whatrequires' in kwargs:
+        return sack.query().available().filter(requires=kwargs['whatrequires'])
+    if 'whatobsoletes' in kwargs:
+        return sack.query().filter(obsoletes=kwargs['whatobsoletes'])
+    if 'requires' in kwargs:
+        pkgs = sack.query().filter(name=kwargs['requires'], latest=1).run()
+        return pkgs[0].requires
+    if 'all' in kwargs and kwargs['all']:
+        return sack.query()
+    raise RuntimeError('unknown query')
 
 
 def old_pkgs():
@@ -60,9 +102,9 @@ def old_pkgs():
         for dependency in ('python(abi) = 2.7',
                            'libpython2.7.so.1.0()(64bit)',
                            'libpython2.7_d.so.1.0()(64bit)'):
-            news = repoquery(version=version,
-                             whatrequires=dependency,
-                             qf='%{NAME} %{EPOCH}:%{VERSION}-%{RELEASE}')
+            pkgs = repoquery(version=version,
+                             whatrequires=dependency)
+            news = {f'{p.name} {p.evr}' for p in pkgs}
             if news:
                 print(f'{len(news)} pkgs require {dependency} in Fedora {version}',
                       file=sys.stderr)
@@ -96,7 +138,7 @@ def removed_pkgs():
     name_versions = defaultdict(set)
     fedoras = old_pkgs()
     last_fedoras = defaultdict(set)
-    new = set(repoquery(all=True, qf='%{NAME}'))
+    new = {pkg.name for pkg in repoquery(all=True)}
     for version in fedoras:
         for name_evr in set(fedoras[version]):
             name, _, evr = name_evr.partition(' ')
@@ -124,11 +166,21 @@ def bump_release(evr):
     ev, _, release = evr.rpartition('-')
     parts = release.split('.')
     release = []
-    for part in parts:
-        if part == '0':
+    for i, part in enumerate(parts):
+        if part == '0' and i != len(parts)-1:
             release.append(part)
         else:
-            release.append(str(int(part) + 1))
+            try:
+                release.append(str(int(part) + 1))
+            except ValueError:
+                if i != len(parts)-1:
+                    release.append(part)
+                    continue
+                match = INTSTART.match(part)
+                if match:
+                    release.append(str(int(match.group(1)) + 1))
+                else:
+                    raise
             release = '.'.join(release)
             return f'{ev}-{release}'
     else:
@@ -143,42 +195,31 @@ def format_obsolete(pkg, evr):
 last_fedoras, max_versions = removed_pkgs()
 
 
-last_known = 'blueman-nautilus'
-#  last_known = None
-
-
 for fed_version in sorted(last_fedoras):
-    print(f'# Python 2 packages removed in Fedora {fed_version+1} but never obsoleted')
+    print(f'\n# Python 2 packages removed in Fedora {fed_version+1} but never obsoleted')
     for pkg in sorted(last_fedoras[fed_version]):
-        if last_known:
-            if last_known == pkg:
-                last_known = None
-            continue
-        print(pkg, file=sys.stderr, end='')
         requires = repoquery(requires=pkg, version=fed_version)
         for require in requires:
             if require not in ('', 'python(abi) = 2.7', 'libpython2.7.so.1.0()(64bit)'):
                 break
         else:
-            print(f'\r# {pkg} only requires Python 2', file=sys.stderr)
+            print(f'# {pkg} only requires Python 2', file=sys.stderr)
             continue
         pkg_version = drop_0epoch(drop_dist(max_versions[pkg]))
         obsoleted_previous = False
         for fedora in range(fed_version, RAWHIDEVER):
-            print('.', file=sys.stderr, end='')
-            whatobsoletes = repoquery(whatobsoletes=f'{pkg} = {pkg_version}', qf='%{NAME}', version=fedora)
+            whatobsoletes = list(repoquery(whatobsoletes=f'{pkg} = {pkg_version}', version=fedora))
             if whatobsoletes:
                 if obsoleted_previous:
-                    print(f'\r# {pkg} obsoleted in Fedora {fedora-1} and {fedora}', file=sys.stderr)
+                    print(f'# {pkg} obsoleted in Fedora {fedora-1} and {fedora}', file=sys.stderr)
                     break
                 obsoleted_previous = True
             else:
                 obsoleted_previous = False
         else:
-            whatobsoletes = repoquery(whatobsoletes=f'{pkg} = {pkg_version}', qf='%{NAME}')
-            if not whatobsoletes or whatobsoletes == ['fedora-obsolete-packages']:
-                print('\r', file=sys.stderr, end='')
+            whatobsoletes = list(repoquery(whatobsoletes=f'{pkg} = {pkg_version}'))
+            if not whatobsoletes or whatobsoletes[0].name == 'fedora-obsolete-packages':
                 print(format_obsolete(pkg, pkg_version))
             else:
-                obs = ', '.join(whatobsoletes)
-                print(f'\r# {pkg} {pkg_version} obsoleted by {obs}', file=sys.stderr)
+                obs = ', '.join(p.name for p in whatobsoletes)
+                print(f'# {pkg} {pkg_version} obsoleted by {obs}', file=sys.stderr)
