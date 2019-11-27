f276cbf9c2be2d481112393430188b858181584e
Miro Hrončok <miro@hroncok.cz>
2019-11-27 17:22:48 +0100

    Final touch to obsolete_packages.py

--- obsolete_packages.py
+++ obsolete_packages.py
@@ -5,16 +5,16 @@ import sys
 from collections import defaultdict
 
 
-FIRST = 14
-EOL = 29
-RAWHIDEVER = 32
+FIRST = 14  # Python 2.7 introduced
+EOL = 29  # the largest Fedora version that is EOL
+RAWHIDEVER = 32  # Fedora rawhide version
 
 DNF_CACHEDIR = '_dnf_cache_dir'
 ARCH = 'x86_64'
 
 INTSTART = re.compile(r'^(\d+).+')
 
-sacks = {}
+sacks = {}  # a global registry of dnf sacks
 
 # modularity problems :(
 # https://bugzilla.redhat.com/show_bug.cgi?id=1636285
@@ -28,8 +28,9 @@ excludepkgs = ('fedora-release,fedora-release-cinnamon,fedora-release-cloud,'
 
 
 def rawhide_sack():
+    """A DNF sack for rawhide, used for queries, cached"""
     try:
-        return sacks[None]
+        return sacks[RAWHIDEVER]
     except KeyError:
         pass
     base = dnf.Base()
@@ -43,11 +44,12 @@ def rawhide_sack():
         enabled=True,
         excludepkgs=excludepkgs)
     base.fill_sack(load_system_repo=False, load_available_repos=True)
-    sacks[None] = base.sack
+    sacks[RAWHIDEVER] = base.sack
     return base.sack
 
 
 def fedora_sack(version):
+    """A DNF sack factory, used for queries, cached"""
     try:
         return sacks[version]
     except KeyError:
@@ -78,8 +80,13 @@ def fedora_sack(version):
 
 
 def repoquery(*args, **kwargs):
-    version = kwargs.pop('version', None)
-    if version is None:
+    """
+    A Python function that somehow works as the repoquery command.
+
+    Only supports --whatrequires, --whatobsoletes, --requires and --all.
+    """
+    version = kwargs.pop('version', RAWHIDEVER)
+    if version == RAWHIDEVER:
         sack = rawhide_sack()
     else:
         sack = fedora_sack(version)
@@ -95,7 +102,13 @@ def repoquery(*args, **kwargs):
     raise RuntimeError('unknown query')
 
 
-def old_pkgs():
+def py2_pkgs():
+    """
+    Returns a dictionary with all Python 2 packages last known in Fedora versions
+
+    keys: Fedora versions
+    values: sets of packages ("name evr" strings) last known in that Fedora
+    """
     fedoras = {}
     for version in range(FIRST, RAWHIDEVER+1):
         fedoras[version] = set()
@@ -120,6 +133,11 @@ def old_pkgs():
 
 
 class SortableEVR:
+    """
+    A way to sort package epoch:version-releases.
+
+    Could be faster if not using subprocess, but is fast enough, so meh.
+    """
     def __init__(self, evr):
         self.evr = evr
 
@@ -135,8 +153,21 @@ class SortableEVR:
 
 
 def removed_pkgs():
+    """
+    Gather all packages that are no longer present in Fedora rawhide.
+
+    Returns 2 dicts:
+
+      last_fedoras:
+        keys: Fedora versions
+        values: sets with package names, no longer in rawhide, last known in that Fedora
+
+      max_versions:
+        keys: package names
+        values: newest (RPM-aware) known epcoh-version-release string
+    """
     name_versions = defaultdict(set)
-    fedoras = old_pkgs()
+    fedoras = py2_pkgs()
     last_fedoras = defaultdict(set)
     new = {pkg.name for pkg in repoquery(all=True)}
     for version in fedoras:
@@ -151,6 +182,7 @@ def removed_pkgs():
 
 
 def drop_dist(evr):
+    """Given epoch:version-release string, return it without a Fedora dist tag"""
     ev, _, release = evr.rpartition('-')
     parts = (part for part in release.split('.') if not part.startswith('fc'))
     release = '.'.join(parts)
@@ -158,11 +190,35 @@ def drop_dist(evr):
 
 
 def drop_0epoch(evr):
+    """
+    Given epoch:version-release string, return it without 0 epoch if possible
+
+    Examples:
+      0:1.2-3 -> 1.2-3
+      1:1.2-3 -> 1:1.2-3
+    """
     epoch, _, vr = evr.partition(':')
     return vr if epoch == '0' else evr
 
 
 def bump_release(evr):
+    """
+    Given epoch:version-release string, return it with bumped release
+
+    First nonzero numeric part (dot separated) of release is incremented by one.
+    If all release parts are zero, the last one is incremented.
+    If there is non nonzero numeric part, increment he last numeric-strating one.
+    Blow up otherwise
+
+    Examples:
+      1:1.2-3 -> 1:1.2-4
+      1.2-0.3 -> 1.2-0.4
+      1.2-0.0.0 -> 1.2-0.0.1
+      1.2-1.bbb.1 -> 1.2-2
+      1.2-aaa.bbb.1 -> 1.2-aaa.bbb.2
+      1.2-1what-2what -> 1.2-1what-3
+      1.2-what -> ValueError
+    """
     ev, _, release = evr.rpartition('-')
     parts = release.split('.')
     release = []
@@ -184,28 +240,39 @@ def bump_release(evr):
             release = '.'.join(release)
             return f'{ev}-{release}'
     else:
-        raise RuntimeError(f'Cannot bump {evr}')
+        raise ValueError(f'Cannot bump {evr}')
 
 
 def format_obsolete(pkg, evr):
+    """
+    For a given package name and version, return the %obsolete line for fedora-obsolete-packages
+    """
     evr = bump_release(evr)
     return f'%obsolete {pkg} {evr}'
 
 
 last_fedoras, max_versions = removed_pkgs()
 
+# Enough of functions, main spaghetti incoming
 
 for fed_version in sorted(last_fedoras):
     print(f'\n# Python 2 packages removed in Fedora {fed_version+1} but never obsoleted')
     for pkg in sorted(last_fedoras[fed_version]):
+
+        # First, ignore packages that only require the 2 things we still have: python(abi) and libpython2
         requires = repoquery(requires=pkg, version=fed_version)
         for require in requires:
             if require not in ('', 'python(abi) = 2.7', 'libpython2.7.so.1.0()(64bit)'):
+                # a different require
                 break
         else:
+            # all requires are within norm
             print(f'# {pkg} only requires Python 2', file=sys.stderr)
             continue
+
         pkg_version = drop_0epoch(drop_dist(max_versions[pkg]))
+
+        # check if the removed package was not obsoleted in 2 consequent releases
         obsoleted_previous = False
         for fedora in range(fed_version, RAWHIDEVER):
             whatobsoletes = list(repoquery(whatobsoletes=f'{pkg} = {pkg_version}', version=fedora))
@@ -216,9 +283,12 @@ for fed_version in sorted(last_fedoras):
                 obsoleted_previous = True
             else:
                 obsoleted_previous = False
+
+        # if not, maybe it is obsoleted in rawhide by something other than fedora-obsolete-packages
         else:
             whatobsoletes = list(repoquery(whatobsoletes=f'{pkg} = {pkg_version}'))
             if not whatobsoletes or whatobsoletes[0].name == 'fedora-obsolete-packages':
+                # finally, yes, we obsolete this
                 print(format_obsolete(pkg, pkg_version))
             else:
                 obs = ', '.join(p.name for p in whatobsoletes)
